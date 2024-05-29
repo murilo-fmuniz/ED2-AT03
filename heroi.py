55073cf3714b1f1b18fd68008d198b8cc8afe18c
murilo-fmuniz <muri.fmuniz@gmail.com>
2024-05-29 19:30:10 -0300

    inicialização do projeto, criação da classe Heroi

---
+++ heroi.py
@@ -0,0 +1,20 @@
+class Heroi:
+    def __init__(self, key, Name, Alignment, Gender, EyeColor, Race, HairColor, Publisher, SkinColor, Height, Weight, Intelligence, Strength, Speed, Durability, Power, Combat, Total):
+        self.key = key
+        self.Name = Name
+        self.Alignment = Alignment
+        self.Gender = Gender
+        self.EyeColor = EyeColor
+        self.Race = Race
+        self.HairColor = HairColor
+        self.Publisher = Publisher
+        self.SkinColor = SkinColor
+        self.Height = Height
+        self.Weight = Weight
+        self.Intelligence = Intelligence
+        self.Strength = Strength
+        self.Speed = Speed
+        self.Durability = Durability
+        self.Power = Power
+        self.Combat = Combat
+        self.Total = Total
---
+++ main.py
@@ -0,0 +1,8 @@
+from heroi import Heroi
+
+
+
+
+if __name__ == "__main__":
+    herois = []
+    print("Heloo, World!")
